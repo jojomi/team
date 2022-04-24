064a60dc74d8601afa1042d87940ceba4da22e26
Johannes Mitlmeier <johannes.mitlmeier@omg.de>
2022-04-24 16:47:31 +0200

    assert: Use ranges for weekdays

--- assert/weekday.go
+++ assert/weekday.go
@@ -3,21 +3,64 @@ package assert
 import (
 	"fmt"
 	"github.com/jojomi/team/exit"
+	"github.com/jojomi/team/ranges"
 	"strings"
 	"time"
 )
 
 func Weekday(args []string) (error, exit.Code) {
 	weekday := time.Now().Weekday()
-	dayString := strings.ToLower(weekday.String())
+	dayStringLong := strings.ToLower(weekday.String())
+	dayStringShort := dayStringLong[0:3]
 
-	// TODO parse -
+	checks := []struct {
+		current string
+		values  []string
+	}{
+		{
+			current: dayStringLong,
+			values:  weekdayListLong,
+		},
+		{
+			current: dayStringShort,
+			values:  weekdayListShort,
+		},
+	}
 
-	// one of?
-	for _, arg := range args {
-		if strings.EqualFold(arg, dayString) {
-			return nil, exit.CodeOkay
+	for _, check := range checks {
+		resolvedArgs, err := ranges.ResolveIndexedRanges(args, check.values)
+		if err != nil {
+			return err, exit.CodeErrorFinal
+		}
+
+		// one of?
+		for _, arg := range resolvedArgs {
+			if strings.EqualFold(arg, check.current) {
+				return nil, exit.CodeOkay
+			}
 		}
 	}
+
 	return fmt.Errorf("today is not in %s", strings.Join(args, ", ")), exit.CodeErrorFinal
 }
+
+var (
+	weekdayListLong = []string{
+		"monday",
+		"tuesday",
+		"wednesday",
+		"thursday",
+		"friday",
+		"saturday",
+		"sunday",
+	}
+	weekdayListShort = []string{
+		"mon",
+		"tue",
+		"wed",
+		"thu",
+		"fri",
+		"sat",
+		"sun",
+	}
+)
